337bcd6ce8bb3956658582b1a89bc9f04f2747a7
Marina Marques <9.marques.marina@gmail.com>
2018-05-13 15:25:41 +0100

    All assets loaded

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// update copyright footer every year
+var today = new Date(),
+    year = today.getFullYear(),
+    copyright = document.getElementById('copyright');
+
+copyright.innerHTML = 'Dharma is a portuguese metal/hardcore band from Lisbon, Portugal © All rights reserved '+ year;
